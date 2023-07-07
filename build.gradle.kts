324d2c32c9bb9164893ce5dd4d59a58b5dd60dde
Tobias Wich <tobias.wich@electrologic.org>
2023-07-07 15:06:52 +0200

    Configure publishing on maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
 plugins {
 	id("org.javamodularity.moduleplugin") version "1.8.12" apply false
 }
+
+group = "org.electrologic"
+version = "0.9.0-SNAPSHOT"
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 	id("org.javamodularity.moduleplugin")
+	// publishing
+	`maven-publish`
+	signing
 }
+
+version = rootProject.version
 
 
 dependencies {
@@ -36,5 +41,72 @@ tasks {
 	test {
 		// Use JUnit Platform for unit tests.
 		useJUnitPlatform()
+	}
+}
+
+artifacts {
+	archives(tasks.getByName("javadocJar"))
+	archives(tasks.getByName("sourcesJar"))
+}
+
+
+signing {
+	useGpgCmd()
+	sign(publishing.publications)
+//	sign(configurations.archives.get())
+}
+
+publishing {
+	repositories {
+		maven {
+			name = "mavenCentral"
+			val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+			credentials {
+				username = System.getenv("OSSRH_USERNAME") ?: project.findProperty("ossrhUsernameEllog") as String?
+				password = System.getenv("OSSRH_PASSWORD") ?: project.findProperty("ossrhPasswordEllog") as String?
+			}
+		}
+	}
+
+	publications {
+		create<MavenPublication>("mavenJava") {
+
+			//from(components["java"])
+			artifact(tasks.getByName("jar"))
+			artifact(tasks.getByName("javadocJar"))
+			artifact(tasks.getByName("sourcesJar"))
+
+			pom {
+				groupId = rootProject.group.toString()
+				artifactId = "uuid4j"
+				packaging = "jar"
+
+				name.set("UUID4J")
+				description.set("UUID4J is a Java library for generating and reading of UUIDs (version 1, 3, 4, 5, 6, and 7).")
+				url.set("https://github.com/sake/uuid4j")
+
+				scm {
+					connection.set("scm:git:https://github.com/sake/uuid4j.git")
+					developerConnection.set("scm:git:ssh://git@github.com/sake/uuid4j.git")
+					url.set("https://github.com/sake/uuid4j")
+				}
+
+				licenses {
+					license {
+						name.set("LGPL-3.0-or-later")
+						url.set("https://www.gnu.org/licenses/lgpl+gpl-3.0.txt")
+					}
+				}
+
+				developers {
+					developer {
+						name.set("Tobias Wich")
+						email.set("tobias.wich@electrologic.org")
+					}
+				}
+			}
+		}
 	}
 }
